5435f732e0bdb50d7e0d7976a0e2e3842fecb7cf
YURIYKALYNBAEV <amber3200@mail.ru>
2023-02-18 19:36:31 +0300

    Курс 3. Итоговая работа по курсу «Введение в веб-разработку». Инициализация

---
+++ src/main/java/me/kalynbaev/onlinestoreofsocksapp/OnlineStoreOfSocksAppApplication.java
@@ -0,0 +1,13 @@
+package me.kalynbaev.onlinestoreofsocksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreOfSocksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreOfSocksAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kalynbaev/onlinestoreofsocksapp/OnlineStoreOfSocksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalynbaev.onlinestoreofsocksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreOfSocksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
